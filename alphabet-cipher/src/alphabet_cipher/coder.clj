5e31843c64b530fb5c324fc0fbb84e7f60eb73ea
Diego Zamboni <diego@zzamboni.org>
2017-01-18 07:45:27 +0100

    Solved alphabet-cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,19 @@
 (ns alphabet-cipher.coder)
 
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map (fn [m k] (char (+ (int \a) (mod (+ (int k) (int m) (- (* 2 (int \a)))) 26))))
+                  (seq message) (cycle (seq keyword)))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map (fn [m k] (char (+ (int \a) (mod (- (int m) (int k)) 26))))
+                  (seq message) (cycle (seq keyword)))))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (let [fullkey (apply str (map (fn [c m] (char (+ (int \a) (mod (- (int c) (int m)) 26))))
+                                (seq cipher) (seq message)))
+        l (count fullkey)] 
+    (->> (range 1 (inc l))
+         (map #(subs fullkey 0 %))
+         (filter #(= fullkey (->> % seq cycle (take l) (apply str))))
+         first)))
 
