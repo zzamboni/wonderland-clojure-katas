59b6c65a8a60af75d601dcb5a68196989a4b6908
Diego Zamboni <diego@zzamboni.org>
2017-01-26 21:21:09 +0100

    Solved doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,19 @@
                (slurp)
                (read-string)))
 
+(defn replace-chr [s i c]
+  (apply str (map-indexed #(if (= %1 i) c %2) s)))
+
+(defn next-words [w]
+  (remove #{w} (filter (set words)
+                       (for [i (range (count w)) c (range 25)]
+                         (replace-chr w i (char (+ c (int \a))))))))
+
+(defn next-sets [lw] (map #(concat lw [%]) (remove (set lw) (next-words (last lw)))))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (or (first (filter #(= word2 (last %))
+                     (apply concat
+                            (take-while not-empty
+                                        (iterate #(mapcat next-sets %) [[word1]])))))
+      []))
